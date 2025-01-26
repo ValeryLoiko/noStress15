c78c6e64ccd09bac4431a3ddd81f2a8d583320e4
Valery Loiko <valery.loiko.ios@gmail.com>
2025-01-26 23:36:19 +0100

    delete storyboard add snapKit

--- Inhale15/ViewController.swift
+++ Inhale15/ViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        // Установка оранжевого фона
+        view.backgroundColor = UIColor.orange
+
+        // Создание и настройка UILabel
+        let label = UILabel()
+        label.text = "Inhale15"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+
+        // Добавление лейбла на экран
+        view.addSubview(label)
+
+        // Установка констрейнтов с помощью SnapKit
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }    }
 
 
 }
