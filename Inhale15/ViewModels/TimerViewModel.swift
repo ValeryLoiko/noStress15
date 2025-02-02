7474d77179a5ad24fd0c18e3409e263034734d5e
Valery Loiko <valery.loiko.ios@gmail.com>
2025-02-02 21:08:25 +0100

    add function for stats/pause button

--- Inhale15/ViewModels/TimerViewModel.swift
+++ Inhale15/ViewModels/TimerViewModel.swift
@@ -8,5 +8,36 @@
 import Foundation
 
 class TimerViewModel {
+    private var timer: Timer?
+    private var startTime: Date?
+    private var accumulatedTime: TimeInterval = 0
+    var elapsedTime: TimeInterval = 0
+    var isTimerRunning = false
+    var onUpdate: ((TimeInterval) -> Void)?
     
+    func startTimer() {
+        guard !isTimerRunning else { return }
+        isTimerRunning = true
+        startTime = Date() - accumulatedTime
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            guard let self = self, let startTime = self.startTime else { return }
+            self.elapsedTime = Date().timeIntervalSince(startTime)
+            self.onUpdate?(self.elapsedTime)
+        }
+    }
+    
+    func pauseTimer() {
+        guard isTimerRunning else { return }
+        isTimerRunning = false
+        timer?.invalidate()
+        accumulatedTime = elapsedTime
+    }
+    
+    func resetTimer() {
+        pauseTimer()
+        elapsedTime = 0
+        accumulatedTime = 0
+        onUpdate?(elapsedTime)
+    }
 }
--- Inhale15/Views/TimerViewController.swift
+++ Inhale15/Views/TimerViewController.swift
@@ -10,80 +10,81 @@ import UIKit
 class TimerViewController: UIViewController {
     
     private let viewModel = TimerViewModel()
-    private let timerLabel = UILabel()
-    private let tableView = UITableView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupUI()
-        bindViewModel()
+      private let timerLabel = UILabel()
+      private let tableView = UITableView()
       
-    }
-    
-    private func setupUI() {
-        view.backgroundColor = UIColor(red: 12/255.0, green: 8/255.0, blue: 67/255.0, alpha: 1.0)
-        
-        timerLabel.font = UIFont.systemFont(ofSize: 48, weight: .bold)
-        timerLabel.textAlignment = .center
-        view.addSubview(timerLabel)
-        
-        timerLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
-        }
-        
-        let startPauseButton = createButton(title: "Старт/Пауза", action: #selector(startPauseTapped))
-        let fifteenSecButton = createButton(title: "15 сек", action: #selector(fifteenSecTapped))
-        let resetButton = createButton(title: "Сброс", action: #selector(resetTapped))
-        let clearHistoryButton = createButton(title: "Очистить историю", action: #selector(clearHistoryTapped))
-        
-        let stack = UIStackView(arrangedSubviews: [startPauseButton, fifteenSecButton, resetButton, clearHistoryButton])
-        stack.axis = .vertical
-        stack.spacing = 10
-        stack.distribution = .fillEqually
-        
-        view.addSubview(stack)
-        stack.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(timerLabel.snp.bottom).offset(40)
-        }
-        
-//        tableView.dataSource = self
-//        view.addSubview(tableView)
-//        tableView.snp.makeConstraints { make in
-//            make.top.equalTo(stack.snp.bottom).offset(20)
-//            make.left.right.bottom.equalToSuperview()
-//        }
-//        tableView.backgroundColor = UIColor(red: 12/255.0, green: 8/255.0, blue: 67/255.0, alpha: 1.0)
-
-    }
-    
-    private func createButton(title: String, action: Selector) -> UIButton {
-        let button = UIButton(type: .system)
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-        button.addTarget(self, action: action, for: .touchUpInside)
-        return button
-    }
-    
-    private func bindViewModel() {
-       
-    }
-    
-    @objc private func startPauseTapped() {
-    
-    }
-    
-    @objc private func fifteenSecTapped() {
-    }
-    
-    @objc private func resetTapped() {
-    }
-    
-    @objc private func clearHistoryTapped() {
-     
-    }
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          setupUI()
+          bindViewModel()
+      }
+      
+      private func setupUI() {
+          view.backgroundColor = UIColor(red: 12/255.0, green: 8/255.0, blue: 67/255.0, alpha: 1.0)
+          
+          timerLabel.font = UIFont.systemFont(ofSize: 48, weight: .bold)
+          timerLabel.textAlignment = .center
+          timerLabel.text = "0.0"
+          view.addSubview(timerLabel)
+          
+          timerLabel.snp.makeConstraints { make in
+              make.centerX.equalToSuperview()
+              make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+          }
+          
+          let startPauseButton = createButton(title: "Старт/Пауза", action: #selector(startPauseTapped))
+          let fifteenSecButton = createButton(title: "15 сек", action: #selector(fifteenSecTapped))
+          let resetButton = createButton(title: "Сброс", action: #selector(resetTapped))
+          let clearHistoryButton = createButton(title: "Очистить историю", action: #selector(clearHistoryTapped))
+          
+          let stack = UIStackView(arrangedSubviews: [startPauseButton, fifteenSecButton, resetButton, clearHistoryButton])
+          stack.axis = .vertical
+          stack.spacing = 10
+          stack.distribution = .fillEqually
+          
+          view.addSubview(stack)
+          stack.snp.makeConstraints { make in
+              make.centerX.equalToSuperview()
+              make.top.equalTo(timerLabel.snp.bottom).offset(40)
+          }
+      }
+      
+      private func createButton(title: String, action: Selector) -> UIButton {
+          let button = UIButton(type: .system)
+          button.setTitle(title, for: .normal)
+          button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+          button.addTarget(self, action: action, for: .touchUpInside)
+          return button
+      }
+      
+      private func bindViewModel() {
+          viewModel.onUpdate = { [weak self] elapsedTime in
+              DispatchQueue.main.async {
+                  self?.timerLabel.text = String(format: "%.1f", elapsedTime)
+              }
+          }
+      }
+      
+      @objc private func startPauseTapped() {
+          if viewModel.isTimerRunning {
+              viewModel.pauseTimer()
+          } else {
+              viewModel.startTimer()
+          }
+      }
+      
+      @objc private func fifteenSecTapped() {
+          // Реализуем позже
+      }
+      
+      @objc private func resetTapped() {
+          viewModel.resetTimer()
+      }
+      
+      @objc private func clearHistoryTapped() {
+          // Реализуем позже
+      }
 }
 
 //extension TimerViewController: UITableViewDataSource {
